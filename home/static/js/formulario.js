7bb3e6feac496d27692dc57fb806f864af02b804
Gerlin Gonzalez <gegovaceo@gmail.com>
2024-08-12 21:55:14 -0500

    Update transport

--- home/static/js/formulario.js
+++ home/static/js/formulario.js
@@ -1,6 +1,7 @@
 const phoneInputField = document.querySelector("#phone");
 const phoneInput = window.intlTelInput(phoneInputField, {
   initialCountry: "co",
+  separateDialCode: true,
   utilsScript:
     "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
 });
