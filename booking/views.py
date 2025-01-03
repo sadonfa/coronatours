57d153e69a3cc78523decd1fbc6c1f13468ee6cf
Gerlin Gonzalez <sadonfa@gmail.com>
2025-01-03 12:22:51 -0500

    Correccion wompi

--- booking/views.py
+++ booking/views.py
@@ -266,8 +266,8 @@ def answer_booking(request, id=False):
     id_wompi = request.GET['id']
     # tour = get_object_or_404(Tours, id=53)
 
-    URL_API =  "https://sandbox.wompi.co/v1/transactions/" + id_wompi
-    # URL_API =  "https://production.wompi.co/v1/transactions/" + id_wompi
+    # URL_API =  "https://sandbox.wompi.co/v1/transactions/" + id_wompi
+    URL_API =  "https://production.wompi.co/v1/transactions/" + id_wompi
 
     response = requests.get(URL_API)
 
