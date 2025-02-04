121b3451f6fd2c20c88ff4425f2b6d32b7d94f7f
Gerlin Gonzalez <sadonfa@gmail.com>
2025-02-03 21:52:44 -0500

    Modo estricto

--- coronatours/settings.py
+++ coronatours/settings.py
@@ -107,6 +107,11 @@ DATABASES = {
         'PASSWORD': env('POSTGRESQL_PASS'),
         'HOST': env('POSTGRESQL_HOST'),
         'PORT': env('POSTGRESQL_PORT'),
+            "OPTION": {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES', innodb_strict_mode=1",
+            'charset': 'utf8mb4',
+            "autocommit": True,
+        }
     }
 }
 
